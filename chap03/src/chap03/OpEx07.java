d22011c97bf485a0227c427a5b03206bf389a65d
gdj97 <goodeegdj90+7@gmail.com>
2025-12-23 15:52:44 +0900

    '20251223-1552'

--- chap03/src/chap03/OpEx07.java
+++ chap03/src/chap03/OpEx07.java
@@ -15,7 +15,13 @@ public class OpEx07 {
 	public static void main(String[] args) {
 		System.out.println("점수를 입력하세요");
 		Scanner scan = new Scanner(System.in);
-		int score = scan.nextInt();
+		int score = scan.nextInt(); //55
 		System.out.println(score +"점:" + ((score >= 60)?"합격":"불합격")+"입니다.");
+		//65점:합격입니다.
+		//55점:불합격입니다.
+		
+		//70점이상은 통과, 60점이상은 재시험, 60점미만은 낙제 출력하기
+		String result = (score >= 70)?"통과":(score >=60)?"재시험":"낙제";
+		System.out.println(score + "점:" + result + "입니다.");
 	}
 }

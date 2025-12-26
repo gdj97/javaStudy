5323615957b5a364967046ef3ed757865e3d849b
gdj97 <goodeegdj90+7@gmail.com>
2025-12-26 15:41:47 +0900

    '20251226-1541'

---
+++ chap05_array/src/test1226/Test01.java
@@ -0,0 +1,14 @@
+package test1226;
+/*
+ * 가로 구구단 출력하기
+ * 2*2=4    3*2=6  ...  9*2=18
+ * 2*3=6
+ * ...
+ * 2*9=18   3*9=27      9*9=81 
+ */
+
+public class Test01 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ chap05_array/src/test1226/Test06.java
@@ -0,0 +1,14 @@
+package test1226;
+/*
+(1)+(1+2)+(1+2+3)+(1+2+3+4)+(1+2+3+4+5)+(1+2+3+4+5+6)+(1+2+3+4+5+6+7)+(1+2+3+4+5+6+7+8)+(1+2+3+4+5+6+7+8+9)+(1+2+3+4+5+6+7+8+9+10)=220
+한줄로 출력하기
+*/
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

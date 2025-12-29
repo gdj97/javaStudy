b73fd5eea761fce80d995d56a1edbd540cdb7283
gdj97 <goodeegdj90+7@gmail.com>
2025-12-29 15:36:37 +0900

    20251229-1536

---
+++ chap05_array/src/test1229/Test05.java
@@ -0,0 +1,22 @@
+package test1229;
+/*
+11*11 크기의 사각형이 있다고 가정할때 다음 결과를 출력되도록 프로그램 작성하기
+
+X         X
+ X       X
+  X     X 
+   X   X  
+    X X   
+     X    
+    X X   
+   X   X  
+  X     X 
+ X       X
+X         X
+ 
+*/
+public class Test05 {
+	public static void main(String[] args) {
+
+	}
+}

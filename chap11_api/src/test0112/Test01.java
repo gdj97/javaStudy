ea90162304e49f1a586a2aec32f7ebc0bbdc7c18
gdj97 <goodeegdj90+7@gmail.com>
2026-01-12 16:02:07 +0900

    '20260111-1602'

---
+++ chap11_api/src/test0112/Test01.java
@@ -0,0 +1,16 @@
+package test0112;
+/*
+ * 두개의 날짜를 입력받아서 두날짜사이의 일수를 출력하기
+  [결과]
+첫번째 날짜를 입력하세요(yyyy-MM-dd)
+2026-01-01
+두번째 날짜를 입력하세요(yyyy-MM-dd)
+2026-01-03
+2026-01-03 -2026-01-01 일자의 차이 : 2
+
+ */
+public class Test01 {
+	public static void main(String[] args) {
+
+	}
+}

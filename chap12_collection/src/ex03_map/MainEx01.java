15bc0b549a80b72ec1b0eeee69327bf938fc7659
gdj97 <goodeegdj90+7@gmail.com>
2026-01-16 15:54:08 +0900

    20260116-1554

--- chap12_collection/src/ex03_map/MainEx01.java
+++ chap12_collection/src/ex03_map/MainEx01.java
@@ -4,7 +4,24 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
+/*
+ * Map 인터페이스 
+ *  - (key,value) 쌍인 객체들의 모임
+ *  - key는 중복불가
+ *  - 구현클래스
+ *    HashMap  : (key,value)의 쌍인 객체. 순서모름.
+ *    Hashtable: (key,value)의 쌍인 객체. 순서모름. 이전 클래스
+ *    TreeMap  : (key,value)의 쌍인 객체. key 기준 정렬됨
+ *  - 주요메서드
+ *    put(key,value) : map 객체에 데이터 추가. 
+ *                     key가 존재하는 경우 value값 수정 
+ *    Set keySet() : key값들만 조회                 
+ *    Collection values() : value값들만 조회
+ *    Set<Map.Entry<K,V>> entrySet() : (key,value) 쌍인 객체들로 조회
+ *    
+ *  - Map.Entry<K,V> : (key,value) 쌍인 객체의 자료형
+ *                     Map 인터페이스의 내부인터페이스형      
+ */
 public class MainEx01 {
 	public static void main(String[] args) {
 		String[] names = {"홍길동","김삿갓","이몽룡","김삿갓"};
@@ -12,7 +29,7 @@ public class MainEx01 {
 		Map<String,Integer> map = new HashMap<>();
 		//map = {"홍길동"=1234,"김삿갓"=9870,"이몽룡"=2350}
 		for(int i=0;i<names.length;i++) {
-			map.put(names[i],nums[i]); //객체 추가
+			map.put(names[i],nums[i]); //객체 추가,수정
 		}
 		System.out.println(map);
 		
